22184beb8917fbeb951676a1df348c557a55dbda
user <29710963+atanasantonov@users.noreply.github.com>
2021-11-01 12:02:01 +0200

    Initial commit.

---
+++ dauen.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * Plugin Name: Disable auto update email notifications
+ * Plugin URI: https://github.com/atanasantonov/dauen
+ * Description: Disable email notifications after auto updates of core, plugins and themes.
+ * Version: 1.0.0
+ * Author: Unax
+ * Author URI: https://unax.org
+ */
+
+// Disable core update email notifications.
+add_filter( 'auto_core_update_send_email', 'dauen_auto_core_update_send_email', 10, 4 );
+
+// Disable plugins update email notifications .
+add_filter( 'auto_plugin_update_send_email', 'dauen_auto_update_send_email', 10, 2 );
+
+// Disable themes update email notifications.
+add_filter( 'auto_theme_update_send_email', 'dauen_auto_update_send_email', 10, 2 );
+
+/**
+ * Disable core update email notifications if result is ok.
+ *
+ * @param bool   $send        Whether to send the email. Default true.
+ * @param string $type        The type of email to send. Can be one of 'success', 'fail', 'critical'.
+ * @param object $core_update The update offer that was attempted.
+ * @param mixed  $result      The result for the core update. Can be WP_Error.
+ *
+ * @return bool
+ */
+function dauen_auto_core_update_send_email( $send, $type, $core_update, $result ) {
+    if ( $result instanceof \WP_Error ) {
+        return true;
+    }
+
+    if ( 'success' !== $type ) {
+        return true;
+    }
+
+    return false;
+}
+
+/**
+ * Disable plugin/theme update email notifications.
+ *
+ * @param bool  $enabled        True if update notifications are enabled, false otherwise.
+ * @param array $update_results The results of update tasks.
+ *
+ * @return bool
+ */
+function dauen_auto_update_send_email( $enabled, $update_results ) {
+    return false;
+}
---
+++ readme.txt
@@ -0,0 +1,14 @@
+=== Disable auto update email notifications ===
+Contributors: atanasantonov
+Donate link: https://www.paypal.com/donate?hosted_button_id=4CZP4EL93G6VJ
+License: GPLv2 or later
+Tags: disable, auto-update, theme, plugin, email, notification, auto, update, updates, stop, automatic
+Requires at least: 5.5
+Tested up to: 5.8.1
+Stable tag: 1.0.0
+Requires PHP: 5.6
+
+Disable email notifications after auto updates of core, plugins and themes.
+
+= 1.0 =
+Initial release
